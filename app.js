cea2396f504f8aa55d1f1bbe644907dc1e6090cd
Jenn Bowers <jenniferbowers03@gmail.com>
2017-06-26 20:57:28 -0400

    fixed bugs, can now begin styling

--- app.js
+++ app.js
@@ -83,6 +83,7 @@ app.get('/game', function(req, res) {
   context.correctLetters = [''];
   context.endingMsg = '';
   context.guessedLetters = [];
+  context.guessesLeft = 8;
   var randomIndex = Math.floor(Math.random() * (words.length));
   var randomWord = words[randomIndex];
   var letters = randomWord.split("");
@@ -98,6 +99,8 @@ app.get('/game', function(req, res) {
   context.letters.shift();
   context.correctLetters = context.correctLetters.concat(correctLetters);
   context.correctLetters.shift();
+  req.session.word = context.letters;
+  req.session.correctWord = context.correctLetters;
   console.log(context);
   res.render('game', context);
 });
@@ -109,11 +112,9 @@ app.post('/login', function(req, res) {
 
   if (user_name) {
     req.session.user = user_name;
-    // console.log(req.session);
   }
 
   if (req.session.user == user_name) {
-    // console.log(req.session.user);
     res.redirect('/game');
   } else {
     console.log('fail');
@@ -124,7 +125,7 @@ app.post('/login', function(req, res) {
 
 app.post('/game', function(req, res) {
   // checking to make sure that the input is only 1 letter
-  req.checkBody('user_input', 'You can only pick one letter at a time').isLength({min: 1, max: 100});
+  req.checkBody('user_input', 'You can only pick one letter at a time').isLength({min: 1, max: 1});
   let errors = req.validationErrors();
   if (errors) {
     context['errors'] = errors;
@@ -164,8 +165,12 @@ app.post('/game', function(req, res) {
     console.log(context);
   }
 
-  // pushes guessed letters into guessed letters array
-  if (!context.guessedLetters.includes(req.body.user_input.toLowerCase())) {
+  // pushes guessed letters into guessed letters array if only one letter, and makes counter not tick for answers with more than one letter
+  if (req.body.user_input.toLowerCase().length > 1) {
+    context.guessesLeft++;
+    req.session.guessesLeft = context.guessesLeft;
+    context.duplicateErrorMsg = ' ';
+  } else if (!context.guessedLetters.includes(req.body.user_input.toLowerCase())) {
     context.guessedLetters.push(req.body.user_input.toLowerCase());
     req.session.guessedLetters = context.guessedLetters;
   }
